c053c83922eedd355c1a1ea80689d628de03b2a6
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2022-06-19 13:01:27 +0530

    Now user registeration is working

--- app.js
+++ app.js
@@ -9,10 +9,31 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
+// For authentication
+const LocalStrategy = require("passport-local");
 // const passportLocal = require("passport-local");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
+  .then(()=>{
+    console.log("connected to DB");
+  })
+  .catch(err=>{
+    console.log("error:", err.message);
+  })
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(passportLocalMongoose); //this will encrypt,hash and salt the data for us.
+
+const User = new mongoose.model("User", userSchema);
+
+// passport.use(new passportLocal(User.authenticate()));
+passport.use(User.createStrategy());
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -30,24 +51,10 @@ app.use(
   })
 );
 
-passport.use(passport.initialize());
-passport.use(passport.session());
-
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-});
-
-userSchema.plugin(passportLocalMongoose); //this will encrypt,hash and salt the data for us.
-
-const User = new mongoose.model("User", userSchema);
-
-// passport.use(new passportLocal(User.authenticate()));
-passport.use(User.createStrategy());
-
-// use static serialize and deserialize of model for passport session support
+// For authentication and session passport local
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -71,22 +78,50 @@ app.get("/secrets", function (req, res) {
   }
 });
 
-app.post("/register", function (req, res) {
-  User.register(
-    { username: req.body.username },
-    req.body.password,
-    (err, user) => {
-      if (err) {
-        console.log(err);
-        res.redirect("/register");
-      } else {
-        passport.authenticate("local")(req, res, function () {
-          res.redirect("/secrets");
-        });
-      }
-    }
-  );
-});
+// Avoid call back hell
+// app.post("/register", function (req, res) {
+//   User.register(
+//     { username: req.body.username },
+//     req.body.password,
+//     (err, user) => {
+//       if (err) {
+//         console.log(err);
+//         res.redirect("/register");
+//       } else {
+//         passport.authenticate("local")(req, res, function () {
+//           res.redirect("/secrets");
+//         });
+//       }
+//     }
+//   );
+// });
+
+// Much better with async and await.
+app.post("/register", async function (req, res){
+  try {
+    const { username, password } = req.body;
+  
+    const user = new User({
+      username,
+      password
+    });
+    const registeredUser = await User.register(user, password);
+    /**
+    * Login the user after registration,
+    * helper function provided by passport.
+    */
+    req.login(registeredUser, err => {
+        if (err) {
+          console.log(err);
+          return res.redirect("/register");
+        };
+        res.redirect("/secrets");
+    })
+  } catch (err){
+    console.log(err);
+    res.redirect("/register");
+  }
+})
 
 app.post("/login", function (req, res) {});
 
